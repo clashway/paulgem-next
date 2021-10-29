d8b4beed000069e048d378c1c6a672654b356a0b
Cory Lashway <clashway@estee.com>
2021-10-29 10:44:06 -0400

    asdf

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,7 @@ import Link from "next/link"
 import NextImage from "./Image"
 
 const Navbar = ({ locale }) => {
+
   return (
     <div className="flex justify-between ml-6 mr-6 mt-4">
       <Link href="/">
@@ -17,8 +18,8 @@ const Navbar = ({ locale }) => {
       </Link>
       <div className="flex items-center">
         {locale === 'en-US' ?
-          <a className="ml-3 font-semibold text-sm text-indigo-500" href="/en-CA">Change to Canada</a>
-          : <a className="ml-3 font-semibold text-sm text-indigo-500" href="/">Change to US</a>}
+          <Link href="/" locale="en-CA"><a className="ml-3 font-semibold text-sm text-indigo-500">Change to Canada</a></Link>
+          : <Link href="/" locale="en-US"><a className="ml-3 font-semibold text-sm text-indigo-500">Change to US</a></Link>}
       </div>
       <div className="flex items-center">
         <a className="ml-3 font-semibold text-sm text-indigo-500" href="/admin">Go to CMS</a>
